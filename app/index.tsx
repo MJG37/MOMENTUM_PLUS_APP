1a81dd744e290ada3938305e84d4bdd2daf3e7ac
Myles Jermayne Gutierrez <23085@apps.eggs.school.nz>
2026-07-22 23:30:27 +1200

    v.0.1.1 - Added the background color to the app/index.tsx file.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25bee1",
+  }
+})
