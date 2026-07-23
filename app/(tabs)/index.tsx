5717b495c599adf405605b1a70c0bd1693c3df1f
Myles Jermayne Gutierrez <23085@apps.eggs.school.nz>
2026-07-23 23:04:42 +1200

    v.0.1.3 - Added new colours tab navigation bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
       <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
       <Text>hi</Text>
 
-      <Link href={"/about"}>Visit about screen</Link>
+    
     </View>
   );
 }
