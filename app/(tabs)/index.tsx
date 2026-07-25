086709ce54bc706fc2864bf6a9c720d7fef09254
Myles Jermayne Gutierrez <23085@apps.eggs.school.nz>
2026-07-25 21:37:49 +1200

    v.0.3.1 - Created dynamic styles to toggle between light/dark mode

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,12 @@
 import { api } from "@/convex/_generated/api";
-import useTheme from "@/hooks/useTheme";
+import useTheme, { ColorScheme } from "@/hooks/useTheme";
 import { useMutation, useQuery } from "convex/react";
-import { Link } from "expo-router";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const {toggleDarkMode} = useTheme(); // Get the toggleDarkMode function from the useTheme hook
+  const {toggleDarkMode, colors} = useTheme(); // Get the toggleDarkMode function from the useTheme hook
 
-  // Fetch the todos from the database using the getTodos query
-  const todos =useQuery(api.todos.getTodos)
-   console.log(todos);
-
-  const addTodo = useMutation(api.todos.addTodo); // Get the addTodo mutation from the Convex API
-  const clearAllTodos = useMutation(api.todos.clearAllTodos); // Get the clearAllTodos mutation from the Convex API
+  const styles = createStyles(colors); // Get createStyles function based on the current color scheme
 
   return (
     <View style={styles.container}>
@@ -21,25 +15,24 @@ export default function Index() {
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>toggle the mode</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => addTodo({ text:"complete maths homework pages 23-25" })}>
-        <Text>Add a new todo</Text>
-      </TouchableOpacity>
-       <TouchableOpacity onPress={() => clearAllTodos()}>
-        <Text>Clear all</Text>
-      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 10,
-  },
-  content: {
-    fontSize: 22,
-  }
-})
+// Function to create dynamic styles based on the current color scheme (light/dark mode)
+const createStyles = (colors:ColorScheme) => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      
+      justifyContent: "center",
+      alignItems: "center",
+      gap: 10,
+      backgroundColor:colors.bg,
+    },
+    content: {
+      fontSize: 22,
+    },
+  });
+  return styles;
+  };
