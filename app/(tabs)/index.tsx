2c72410e34f7572dd7ea0253adc0dd6b78bd50f4
Myles Jermayne Gutierrez <23085@apps.eggs.school.nz>
2026-07-25 16:36:13 +1200

    v.0.3.0 - Created new task buttons and Setup backend using Convex, (new functions, mutations and queries)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,31 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { Link } from "expo-router";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const {toggleDarkMode} = useTheme(); // Get the toggleDarkMode function from the useTheme hook
+
+  // Fetch the todos from the database using the getTodos query
+  const todos =useQuery(api.todos.getTodos)
+   console.log(todos);
+
+  const addTodo = useMutation(api.todos.addTodo); // Get the addTodo mutation from the Convex API
+  const clearAllTodos = useMutation(api.todos.clearAllTodos); // Get the clearAllTodos mutation from the Convex API
+
   return (
     <View style={styles.container}>
       <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
       <Text>hi</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>toggle the mode</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => addTodo({ text:"complete maths homework pages 23-25" })}>
+        <Text>Add a new todo</Text>
+      </TouchableOpacity>
+       <TouchableOpacity onPress={() => clearAllTodos()}>
+        <Text>Clear all</Text>
       </TouchableOpacity>
     </View>
   );
---
+++ convex/todos.ts
@@ -0,0 +1,70 @@
+import { mutation, query } from "./_generated/server";
+import { ConvexError, v} from "convex/values";
+
+export const getTodos = query({
+    handler: async (ctx) => {
+        const todos = await ctx.db.query("todos").order("desc").collect()
+        return todos;
+    },
+});
+
+// responsible for adding a new todo to the database
+export const addTodo = mutation({
+    args: {text:v.string()},
+    handler: async(ctx,args) => {
+        const todoID = await ctx.db.insert("todos", {
+            text: args.text,
+            isCompleted: false,
+        });
+
+        return todoID;
+    },
+});
+
+// responsible for toggling the "isCompleted" status of a todo in the database
+export const toggleTodo = mutation({
+    args: {id:v.id("todos")},
+    handler: async(ctx,args) => {
+        const todo = await ctx.db.get(args.id)
+        if(!todo) throw new ConvexError("Todo not found")
+
+        await ctx.db.patch(args.id, {
+            isCompleted: !todo.isCompleted,
+        })
+    }
+})
+
+// responsible for deleting A todo from the database
+export const deleteTodo = mutation({
+    args: {id:v.id("todos")},
+    handler: async (ctx,args) => {
+        await ctx.db.delete(args.id);
+    },
+});
+
+// responsible for updating a todo in the database
+export const updateTodo = mutation({
+    args: {
+        id: v.id("todos"),
+        text: v.string(),
+    },
+    handler: async (ctx, args) => {
+        await ctx.db.patch(args.id, { 
+            text: args.text, 
+        });
+    },
+});
+
+// responsible clearing ALL todos in the database
+export const clearAllTodos = mutation({
+    handler: async(ctx) => {
+       const todos = await ctx.db.query("todos").collect();
+
+       // Delete ALL todos
+       for (const todo of todos) {
+        await ctx.db.delete(todo._id);
+        }
+
+        return { deleteCount: todos.length };
+    },
+});
