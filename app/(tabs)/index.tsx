6db00c82964e78efa74c403df11c2e3fe9b74aa1
Myles Jermayne Gutierrez <23085@apps.eggs.school.nz>
2026-07-27 22:29:26 +1200

    v.0.6.0 - Created "loading spinner" (when app is refreshed/processing commands) component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,26 @@
 import { createHomeStyles } from "@/assets/styles/home.styles";
 import Header from "@/components/Header";
+import LoadingSpinner from "@/components/LoadingSpinner";
 import TodoInput from "@/components/TodoInput";
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useQuery } from "convex/react";
 import { LinearGradient } from 'expo-linear-gradient';
 import { StatusBar, Text, TouchableOpacity } from "react-native";
+import { FlatList } from "react-native-reanimated/lib/typescript/Animated";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme(); // Get the toggleDarkMode function from the useTheme hook
 
   const homeStyles = createHomeStyles(colors); // Get createHomeStyles function based on the current color scheme
+
+    const todos = useQuery(api.todos.getTodos); // Get the useQuery function to fetch any new todos
+    
+    const isLoading = todos === undefined
+
+    if(isLoading) return <LoadingSpinner />;
+
 
   return (
     <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
@@ -19,9 +30,14 @@ export default function Index() {
         <Header />
 
         <TodoInput/>
-        <TouchableOpacity onPress={toggleDarkMode}>
-          <Text>toggle the mode</Text>
-        </TouchableOpacity>
+
+        <FlatList
+          data={todos}
+          renderItem={renderTodoItem}
+          keyExtractor={(item) => item._id}
+          style={homeStyles.todoList}
+          contentContainerStyle={homeStyles.todoListContent}
+        />
       </SafeAreaView>
     </LinearGradient>
   );
