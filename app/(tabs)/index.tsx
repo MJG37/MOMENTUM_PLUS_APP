a5afc8b550e73527b9b80a9abdf184c1b6a1fb67
Myles Jermayne Gutierrez <23085@apps.eggs.school.nz>
2026-07-28 20:18:40 +1200

    v.0.7.0 - Added blank task boxes and "check" button function for completion of tasks

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,12 +3,15 @@ import Header from "@/components/Header";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import TodoInput from "@/components/TodoInput";
 import { api } from "@/convex/_generated/api";
+import { Doc, Id } from "@/convex/_generated/dataModel";
 import useTheme from "@/hooks/useTheme";
-import { useQuery } from "convex/react";
+import { Ionicons } from "@expo/vector-icons";
+import { useMutation, useQuery } from "convex/react";
 import { LinearGradient } from 'expo-linear-gradient';
-import { StatusBar, Text, TouchableOpacity } from "react-native";
-import { FlatList } from "react-native-reanimated/lib/typescript/Animated";
+import { Alert, FlatList, StatusBar, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+type Todo = Doc<"todos">
 
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme(); // Get the toggleDarkMode function from the useTheme hook
@@ -16,10 +19,47 @@ export default function Index() {
   const homeStyles = createHomeStyles(colors); // Get createHomeStyles function based on the current color scheme
 
     const todos = useQuery(api.todos.getTodos); // Get the useQuery function to fetch any new todos
-    
+    const toggleTodo = useMutation(api.todos.toggleTodo)
+
     const isLoading = todos === undefined
 
     if(isLoading) return <LoadingSpinner />;
+
+    // Responsible for the "check" function when a task is completed
+    const handleToggleTodo = async (id:Id<"todos">) => {
+      try {
+        await toggleTodo({id})
+      } catch (error) {
+        console.log("Error toggling todo", error);
+        Alert.alert("Error", "Failed to toggle todo");
+      }
+    };
+
+    // Responsible for the todo "boxes" and buttons
+    const renderTodoItem =  ({item}: {item:Todo}) => {
+      return (
+        <View style={homeStyles.todoItemWrapper}>
+          <LinearGradient colors={colors.gradients.surface}
+            style={homeStyles.todoItem}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1}} 
+          >
+            <TouchableOpacity
+            style={homeStyles.checkbox}
+            activeOpacity={0.7}
+            onPress={() => handleToggleTodo(item._id)}>
+              <LinearGradient
+                colors={item.isCompleted ? colors.gradients.success : colors.gradients.muted}
+                style={[homeStyles.checkboxInner, { borderColor: item.isCompleted ? "transparent" : colors.border},]}>
+                {item.isCompleted && <Ionicons name="checkmark" size={18} color="#fff" />}
+              </LinearGradient>
+
+            </TouchableOpacity>
+
+          </LinearGradient>
+        </View>
+      );
+    };
 
 
   return (
@@ -36,7 +76,7 @@ export default function Index() {
           renderItem={renderTodoItem}
           keyExtractor={(item) => item._id}
           style={homeStyles.todoList}
-          contentContainerStyle={homeStyles.todoListContent}
+          contentContainerStyle={homeStyles.todoListContent }
         />
       </SafeAreaView>
     </LinearGradient>
